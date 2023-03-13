b2f573b998e7761136bdba39b30b4796595986c5
JuhoErvasti <juho.ervasti@gispo.fi>
2023-03-13 16:03:55 +0200

    Bug fixes, translation, icons, refactoring

--- infrao/ui/ask_password.py
+++ infrao/ui/ask_password.py
@@ -1,11 +1,14 @@
 from qgis.PyQt import QtWidgets
 
 from ..qgis_plugin_tools.tools.resources import load_ui
+
 FORM_CLASS = load_ui('ask_password_dialog.ui')
 
 
 class DbAskPasswordDialog(QtWidgets.QDialog, FORM_CLASS):
     onCloseHandler = None
+
+    result_ = ""
 
     def __init__(self, parent=None):
         """Constructor."""
@@ -14,14 +17,21 @@ class DbAskPasswordDialog(QtWidgets.QDialog, FORM_CLASS):
         super(DbAskPasswordDialog, self).__init__(parent)
         self.setupUi(self)
 
-        self.cancelButton.clicked.connect(self.close)
         self.okButton.clicked.connect(self.check_match)
+        self.closeButton.clicked.connect(self.close_self)
+
 
 
     def check_match(self):
         if len(self.pwdLineEdit.text()) <= 8:
-            self.pwdNoMatchLabel.setText("Password has to be at least 8 characters long.")
+            self.pwdNoMatchLabel.setText("Salasanan pitää olla vähintään 8 merkkiä pitkä.")
         elif not (self.pwdLineEdit.text() == self.pwd2LineEdit.text()):
-            self.pwdNoMatchLabel.setText("Passwords do not match.")
+            self.pwdNoMatchLabel.setText("Salasanat eivät täsmää.")
         else:
+            self.result_ = self.pwdLineEdit.text()
             self.close()
+        
+
+    def close_self(self):
+        self.result_ = ""
+        self.close()
