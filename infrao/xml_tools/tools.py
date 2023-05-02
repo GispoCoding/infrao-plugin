13b88c933a109054540f59ad6af3c8beb6cb4289
JuhoErvasti <juho.ervasti@gispo.fi>
2023-05-02 11:17:56 +0300

    Add initial version of xml exporting

--- infrao/xml_tools/tools.py
+++ infrao/xml_tools/tools.py
@@ -16,7 +16,6 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with infrao-plugin.  If not, see <https://www.gnu.org/licenses/>.
-import xml.etree.ElementTree as ET
 
 CORE_NS = "infrao"
 
